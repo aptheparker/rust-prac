511b9c7f64152acac528e5d900b7e768aa386f07
aptheparker <apapap1119@g.skku.edu>
2023-11-12 15:04:32 +0900

    mutable let const prac

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-
-    // variable initialization with condition
-    let x: i32; 
-
-    if true {
-        x = 1;
-    }
-    else{
-        x = 2;
-    }
-    println!("x is {}", x);
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
 }
