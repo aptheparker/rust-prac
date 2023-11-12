a1f0beb93885de6fda08108c7fb0361396543d48
aptheparker <apapap1119@g.skku.edu>
2023-11-12 11:41:49 +0900

    variable initialization with condition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    // variable initialization with condition
+    let x: i32; 
+
+    if true {
+        x = 1;
+    }
+    else{
+        x = 2;
+    }
+    println!("x is {}", x);
 }
